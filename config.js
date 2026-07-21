58de0ba983a99fd02682934812a472e3caa006b3
Elbalta <162491303+Elbalta@users.noreply.github.com>
2026-07-21 00:39:32 -0400

    feat: load product catalog from Supabase

---
+++ config.js
@@ -0,0 +1,5 @@
+window.APP_CONFIG = Object.freeze({
+  SUPABASE_URL: 'https://sykfeqccxqthrqjljpgx.supabase.co',
+  SUPABASE_PUBLISHABLE_KEY: 'sb_publishable_FKyPHW3fsi7L4q2JbbDLsA_pml3Lvut',
+  WHATSAPP_NUMBER: '569XXXXXXXX'
+});
